4d829d0cff3c4bc9ae00372609a0c13ea3a58639
jorgejunior618 <jorgejunior618@gmail.com>
2023-03-06 11:18:24 -0300

    leitura de variaveis de ambiente

--- src/aws/index.ts
+++ src/aws/index.ts
@@ -1,6 +1,7 @@
 import aws from 'aws-sdk';
 
-aws.config.loadFromPath('../../aws-config.json');
-aws.config.update({region: 'us-east-1'});
+aws.config.update({
+  region: 'us-east-1'
+});
 
 export default new aws.Rekognition();
